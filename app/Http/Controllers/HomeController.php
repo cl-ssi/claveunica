d389a82554ed74ea884a7fd33647b51707643a12
AquaroTorres <alvarotorres@gmail.com>
2020-04-03 15:22:39 -0300

    asdf

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,8 @@ class HomeController extends Controller
         $response = Http::get($url);
         $stat = $response->json();
         //$json = '{"total":429,"positives":17,"pending":149,"negatives":263}';
-        //return json_decode($json,true);
+        printe_r($stat);
+        die();
         return view('welcome',compact('stat'));
     }
 
